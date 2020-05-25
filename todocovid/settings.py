b599cb37efb470a6a4beae2b7a9e081ce6cdb80d
alzseven <alskdjfhg7455@naver.com>
2020-05-25 23:33:09 +0900

    idn

--- todocovid/settings.py
+++ todocovid/settings.py
@@ -89,8 +89,8 @@ WSGI_APPLICATION = 'todocovid.wsgi.application'
 # }
 DATABASES = {
     "default": {
-        "ENGINE" : "django.db.backends.postgresql_psycopg2",
-        "NAME": os.path.join(BASE_DIR, "postgresql_psycopg2")
+        "ENGINE" : "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
     }
 }
 
