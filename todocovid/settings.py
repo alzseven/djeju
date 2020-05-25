9ff8ee075bc0306b1dc951f7d2b7c309d46f990f
alzseven <alskdjfhg7455@naver.com>
2020-05-25 21:06:27 +0900

    timezone

--- todocovid/settings.py
+++ todocovid/settings.py
@@ -112,9 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko'
 
-TIME_ZONE = 'us/eastern'
+TIME_ZONE = 'asia/seoul'
 
 USE_I18N = True
 
