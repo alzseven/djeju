03b8d340f151278eddc771503e697b4ce20e9607
alzseven <alskdjfhg7455@naver.com>
2020-05-25 21:03:07 +0900

    reigon

--- todocovid/settings.py
+++ todocovid/settings.py
@@ -112,9 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'ko'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia\Seoul'
+TIME_ZONE = 'us/eastern'
 
 USE_I18N = True
 
