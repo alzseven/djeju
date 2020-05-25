d0696350e1f624f54a1256c4d73f8c2d1684621e
alzseven <alskdjfhg7455@naver.com>
2020-05-26 07:27:17 +0900

    view fixed..

--- map/views.py
+++ map/views.py
@@ -7,7 +7,7 @@ def map(request):
     return render(request,'map/map.html',{})
 
 def curmap(request,lat,lng):
-    location = Location.objects.filter(lat=float(lat), lng=float(lng))
+    location = Location.objects.Create(lat=float(lat), lng=float(lng))
 
     if len(location) > 0:
         loc = location[0]
