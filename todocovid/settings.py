9d5b80de497ccbaf1722d018c513a34f992e7223
alzseven <alskdjfhg7455@naver.com>
2020-06-05 04:15:28 +0900

    dj-dburl

--- todocovid/settings.py
+++ todocovid/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-# import dj_database_url
 import django_heroku
 import os
 
@@ -161,6 +160,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 django_heroku.settings(locals())
 
