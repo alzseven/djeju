ec913eb8d9a614f2275be966fab78c08533c3c37
alzseven <alskdjfhg7455@naver.com>
2020-06-08 23:17:52 +0900

    admins etc

--- info/admin.py
+++ info/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from django.contrib.gis.admin import OSMGeoAdmin
+from .models import Sido
 # Register your models here.
+
+@admin.register(Sido)
+class SidoAdmin(OSMGeoAdmin):
+    list_display = ('gubun', 'inc_dec','create_dt')
