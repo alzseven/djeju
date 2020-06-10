50ea48627903de6dd8914dc1ea4228c2347a0a26
alzseven <alskdjfhg7455@naver.com>
2020-06-11 08:15:18 +0900

    sido working

--- info/views.py
+++ info/views.py
@@ -19,8 +19,17 @@ def sidoview(request):
     cur_sido = result['documents'][0]['address']["region_1depth_name"][0:2] #?
 
     queryset = Sido.objects.filter(gubun__contains=cur_sido)
-    data = list(queryset.values())
+    values = list(queryset.values("gubun","defCnt","isol_clear_cnt","death_cnt"))
 
+    data = Context(
+        {
+            "keys":["시도명","확진","격리해제","사망"]
+            "시도명": values["gubun"]
+            "확진": values["defCnt"]
+            "격리해제": values["isol_clear_cnt"]
+            "사망": values["death_cnt"]
+        }
+    )
     # JsonSerializer = serializers.get_serializer("json")
     # json_serializer = JsonSerializer()
     # json_serializer.serialize(queryset)
