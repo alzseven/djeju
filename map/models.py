443aeb821d2ca5c3045049bf088953d873d06201
alzseven <alskdjfhg7455@naver.com>
2020-06-16 03:36:34 +0900

    fix model

--- map/models.py
+++ map/models.py
@@ -7,13 +7,13 @@ class Hospitals(gismodels.Model):
     sgguNm = gismodels.CharField(max_length=400)      #시군구명
     location = gismodels.PointField()
     yadmNm = gismodels.CharField(max_length=200)      #기관명 = 병원이름
-    # hospTyTpCd = gismodels.CharField(max_length=2,null=True,blank=True)  #선정유형 = 국민안심병원 선정유형
+    hospTyTpCd = gismodels.CharField(max_length=2,null=True,blank=True)  #선정유형 = 국민안심병원 선정유형
     isReliefhos = gismodels.BooleanField() 
     isInspect = gismodels.BooleanField()
     isTriage = gismodels.BooleanField()
     telno = gismodels.CharField(max_length=30,primary_key=True)       #전화번호
     adtFrDd = gismodels.CharField(max_length=8)     #운영가능일자
-    spclAdmTyCd = gismodels.CharField(max_length=2) #구분코드
+    # spclAdmTyCd = gismodels.CharField(max_length=2) #구분코드
 
     def __str__(self):
          return self.yadmNm
