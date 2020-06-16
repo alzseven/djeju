bf708497205598ce45b6960c1e566dec876ab516
alzseven <alskdjfhg7455@naver.com>
2020-06-16 17:14:45 +0900

    updater

--- scripts/hosController.py
+++ scripts/hosController.py
@@ -8,7 +8,7 @@ from map.models import Hospitals
 def run():
     CREATE_CODE = 1
     # READ_CODE = 2
-    # UPDATE_CODE = 3
+    UPDATE_CODE = 3
     DELETE_CODE = 4
     RESET_CODE = 5
 
@@ -17,8 +17,8 @@ def run():
         createhos()
     # elif (args==2):
     #     pass
-    # elif (args==3):
-    #     pass
+    elif (args==UPDATE_CODE):
+        updatehos()
     elif (args==DELETE_CODE):
         pass
     elif (args==RESET_CODE):
@@ -38,8 +38,8 @@ def createhos():
     # _spclAdmTyCd = ""
 
     print("총 데이터 수: " + totalCount)
-    input_min = int(input("입력을 시작할 페이지: (페이지 당 데이터 100개"))
-    input_max = int(input("입력을 종료할 페이지: (페이지 당 데이터 100개"))
+    input_min = int(input("입력을 시작할 페이지: (페이지 당 데이터 100개)"))
+    input_max = int(input("입력을 종료할 페이지: (페이지 당 데이터 100개)"))
     if(input_max > (int(totalCount)//numOfRows) + 2 or input_min < 1):
         print("잘못된 입력입니다.")
         sys.exit(1)
@@ -144,3 +144,42 @@ def createhos():
                         telno = _telno,
                         adtFrDd = _adtFrDd,).save()
                         #spclAdmTyCd = _spclAdmTyCd,
+
+def updatehos():
+    numOfRows = 100
+    key = "j%2BnuUay451ipAStppt2Uh7XE3aAUvC%2FtxdLMMHEreI7KR%2FY0%2B0%2BIAsODyasKyftwZXHwQ8SNTxD2QY5y2W8aXw%3D%3D"
+    preUrl = "http://apis.data.go.kr/B551182/pubReliefHospService/getpubReliefHospList?ServiceKey=" + key + "&pageNo=1&numOfRows=10"
+    preReq = requests.get(preUrl).content
+    prexmlObj = xmltodict.parse(preReq)
+    totalCount = prexmlObj['response']['body']['totalCount']
+
+    # _spclAdmTyCd = ""
+
+    print("총 데이터 수: " + totalCount)
+    input_min = int(input("입력을 시작할 페이지: (페이지 당 데이터 100개)"))
+    input_max = int(input("입력을 종료할 페이지: (페이지 당 데이터 100개)"))
+    if(input_max > (int(totalCount)//numOfRows) + 2 or input_min < 1):
+        print("잘못된 입력입니다.")
+        sys.exit(1)
+
+    # 1,(int(totalCount)//numOfRows)+2
+    for page_num in range(input_min, input_max):
+        _hospTyTpCd = ""
+        
+        relihosUrl = "http://apis.data.go.kr/B551182/pubReliefHospService/getpubReliefHospList?ServiceKey=" \
+            + key +"&pageNo=" + str(page_num) + "&numOfRows=" + str(numOfRows) + "&spclAdmTyCd=A0"
+        reliReq = requests.get(relihosUrl).content
+        relixmlObj = xmltodict.parse(reliReq)
+        reliData = relixmlObj['response']['body']['items']['item']
+        
+        
+        for i in range(len(reliData)):
+            if 'hospTyTpCd' in reliData[i].keys():
+                _hospTyTpCd = reliData[i]['hospTyTpCd']
+            _telno = reliData[i]['telno']
+            
+            # if reliData[i]['spclAdmTyCd']
+            if Hospitals.objects.filter(telno = _telno).exists():
+                if reliData[i]['spclAdmTyCd'] == "A0":
+                    Hospitals.objects.filter(telno = _telno).update(hospTyTpCd = _hospTyTpCd)
+                    continue
